49b94b86a9b3defae3fca8f737b212c2974334f5
GaoWei <qazmichaelgw@gmail.com>
2016-09-09 13:08:23 +0800

    change target

--- runros.sh
+++ runros.sh
@@ -9,7 +9,9 @@
 # See: http://wiki.ros.org/docker/Tutorials/GUI
 #TARGET=osrf/ros:indigo-desktop-full-nvidia
 #TARGET=ros:indigo-ros-base
-TARGET=qazmichaelgw/deepnav_docker
+#TARGET=qazmichaelgw/deepnav_docker
+TARGET=qazmichaelgw/deepnav_tensorflow
+#TARGET=gcr.io/tensorflow/tensorflow:latest-gpu
 #export MYNVIDIA=$(curl -s http://localhost:3476/v1.0/docker/cli?dev=0\&vol=nvidia_driver)
 #export MYNVIDIA="-e LD_LIBRARY_PATH=/usr/local/nvidia/lib64 --volume=nvidia_driver_361.42:/usr/local/nvidia:ro --device=/dev/nvidia0 --device=/dev/nvidiactl --device=/dev/nvidia-uvm"
 #export MYNVIDIA="-e LD_LIBRARY_PATH=/usr/local/nvidia/lib64 --volume=nvidia_driver_361.42:/usr/local/nvidia:ro --device=/dev/nvidia0 --device=/dev/nvidiactl --device=/dev/nvidia-uvm"
@@ -17,7 +19,7 @@ TARGET=qazmichaelgw/deepnav_docker
 xhost + > /dev/zero
 #--device /dev/nvidia0 --device /dev/nvidiactl --device /dev/bus/usb:/dev/bus/usb:rwm"
 # -v /usr/bin/X11/xeyes:/usr/bin/X11/xeyes
-nvidia-docker run -it --rm --net host -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix -v /media/psl-ctg/Data:/workspace -e DISPLAY=$DISPLAY $TARGET
+nvidia-docker run -it --rm --net host -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix -v /media/psl-ctg/Data:/workspace -e DISPLAY=$DISPLAY $TARGET bash
 #TODO fix 
 #TODO add 
 #source /opt/ros/indigo/setup.bash
