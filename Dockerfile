499b5e35ebde3c1f807efee8128f41fdb1f39d53
GaoWei <qazmichaelgw@gmail.com>
2017-04-18 16:01:03 +0800

    fix the bug of rviz

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM nvidia/cuda:8.0-devel-ubuntu14.04
+FROM nvidia/cuda:8.0-runtime-ubuntu14.04
 LABEL maintainer "NVIDIA CORPORATION <cudatools@nvidia.com>"
 
 RUN echo "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1404/x86_64 /" > /etc/apt/sources.list.d/nvidia-ml.list
@@ -7,6 +7,5 @@ ENV CUDNN_VERSION 5.1.10
 LABEL com.nvidia.cudnn.version="${CUDNN_VERSION}"
 
 RUN apt-get update && apt-get install -y --no-install-recommends \
-            libcudnn5=$CUDNN_VERSION-1+cuda8.0 \
-            libcudnn5-dev=$CUDNN_VERSION-1+cuda8.0 && \
+            libcudnn5=$CUDNN_VERSION-1+cuda8.0 && \
     rm -rf /var/lib/apt/lists/*
--- runros.sh
+++ runros.sh
@@ -11,7 +11,7 @@
 #TARGET=ros:indigo-ros-base
 #TARGET=qazmichaelgw/deepnav_docker
 #TARGET=qazmichaelgw/deepnav_ros
-TARGET=sdc
+TARGET=qazmichaelgw/sdc
 #TARGET=gcr.io/tensorflow/tensorflow:latest-gpu
 #export MYNVIDIA=$(curl -s http://localhost:3476/v1.0/docker/cli?dev=0\&vol=nvidia_driver)
 #export MYNVIDIA="-e LD_LIBRARY_PATH=/usr/local/nvidia/lib64 --volume=nvidia_driver_361.42:/usr/local/nvidia:ro --device=/dev/nvidia0 --device=/dev/nvidiactl --device=/dev/nvidia-uvm"
@@ -20,7 +20,9 @@ TARGET=sdc
 xhost + > /dev/zero
 #--device /dev/nvidia0 --device /dev/nvidiactl --device /dev/bus/usb:/dev/bus/usb:rwm"
 # -v /usr/bin/X11/xeyes:/usr/bin/X11/xeyes
-nvidia-docker run -it --rm --net host -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix -v /media/gaowei/Blade/linux_data/umv:/umv_data -e DISPLAY=$DISPLAY $TARGET bash
+#nvidia-docker run -it --rm --net host -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix -v /media/gaowei/Blade/linux_data/umv:/umv_data -e DISPLAY=$DISPLAY $TARGET bash
+# fix the rviz bug by this new command
+nvidia-docker run --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -env="QT_X11_NO_MITSHM=1" -v /dev/video0:/dev/video0 -v /tmp/.X11-unix:/tmp/.X11-unix:ro -it --rm -p 8888:8888 -p 6006:6006 -v /media/gaowei/Blade/linux_data/umv:/umv_data $TARGET bash
 #TODO fix 
 #TODO add 
 #source /opt/ros/indigo/setup.bash
